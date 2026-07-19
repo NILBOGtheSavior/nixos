224557a64b94db7331aeb8c8a67b5f3d86db194e
NILBOGtheSavior <naztechmedia@gmail.com>
2026-07-19 12:06:41 -0500

    Added home manager and configured zsh

--- flake.nix
+++ flake.nix
@@ -3,14 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
       blade = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hosts/blade/configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
         ];
       };
     };
--- users/nilbog.nix
+++ users/nilbog.nix
@@ -9,4 +9,48 @@
       tree
     ];
   };
+
+  home-manager.users.nilbog = {
+    home.username = "nilbog";
+    home.homeDirectory = "/home/nilbog";
+    home.stateVersion = "25.05";
+
+    programs.starship = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    programs.zsh = {
+      enable = true;
+
+      history = {
+        size = 1000;
+        save = 1000;
+        path = "$HOME/.histfile";
+      };
+
+      shellAliases = {
+        ls = "lsd --color=auto";
+        la = "lsd -a";
+        ll = "lsd -l";
+        lla = "lsd -la";
+        lt = "lsd --tree";
+        cat = "bat --paging=never";
+        grep = "rg --color=auto";
+        zj = "zellij";
+      };
+
+      antidote = {
+        enable = true;
+        plugins = [
+          ''
+            zsh-users/zsh-autosuggestions
+            zsh-users/zsh-syntax-highlighting
+            djui/alias-tips
+          ''
+        ];
+      };
+    };
+    programs.home-manager.enable = true;
+  };
 }
